e0f5149243c1d363e38bb424490db7422368a4dd
Kyle Bippus <kbippus@umd.edu>
2023-08-29 13:17:56 -0400

    Try adding base public path to solve 404s

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/western-orthodox-prayer/",
   plugins: [vue(), legacy(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
